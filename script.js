a206f59d1ec8330e3138dd936ce6b16c71709674
Mangesh Bodke <mangeshbodke582@gmail.com>
2025-04-08 21:41:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 //your JS code here. If required.
+
+function daysOfAYear(a) {
+  if (a % 400 === 0) {
+    return 366; // Leap year
+  } else if (a % 100 === 0) {
+    return 365; // Not a leap year
+  } else if (a % 4 === 0) {
+    return 366; // Leap year
+  } else {
+    return 365; // Not a leap year
+  }
+}
+
+console.log(daysOfAYear(500)); // Output: 365
